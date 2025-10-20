7606630ea9395d38cd909938024a29c119c3b758
PamonhaMaker <gabrizdiniz@gmail.com>
2025-10-20 16:40:38 -0300

    Add PHP script for integer array example
    Create a PHP script to define and display an array of integers.

--- exercicio17.php
+++ exercicio17.php
@@ -1,1 +1,25 @@
+<?php
+    /**************************
+    Curso: Engenharia de Software
+    Disciplina: Linguagem e Técnicas de Programacão
+    Professor: Flores
+    Turma: ESOFT-2A
+    Componentes:
+    25142481-2 - Isabelli Azevedo
+    25324764-2- Gabriel Diniz
+    25016187-2- Michel Bueno Chacon 
+    25127107-2-Tiago Joslin
+    Data: 08 de Outubro de 2025
+    Descritivo: Array Homogêneo: Crie um array com 10 números inteiros.
+    *********/
 
+// Definindo um array homogêneo de inteiros
+$num = [10, 25, 7, 42, 33, 18, 60, 5, 91, 12];
+
+// Exibindo os valores do array
+echo "Array de números inteiros:\n";
+
+foreach ($num as $num) {
+    echo $num . "\n";
+}
+?>
