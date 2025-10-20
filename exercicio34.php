0b46ae54523de47e1ab7fe268fe3d2b4d0b360c6
PamonhaMaker <gabrizdiniz@gmail.com>
2025-10-20 17:20:50 -0300

    Update exercicio34.php

--- exercicio34.php
+++ exercicio34.php
@@ -1,1 +1,27 @@
+<?php
+    /**************************
+    Curso: Engenharia de Software
+    Disciplina: Linguagem e Técnicas de Programacão
+    Professor: Flores
+    Turma: ESOFT-2A
+    Componentes:
+    25142481-2 - Isabelli Azevedo
+    25324764-2- Gabriel Diniz
+    25016187-2- Michel Bueno Chacon 
+    25127107-2-Tiago Joslin
+    Data: 20 de Outubro de 2025
+    Descritivo: Elabore um procedimento verificarParImpar que receba um número e imprima se ele é "Par" ou "Ímpar".
+    ***************************/
+$numero = 3
 
+function verificarParImpar($numero) {
+    if ($numero % 2 == 0) {
+        echo "O número $numero é Par.";
+    } else {
+        echo "O número $numero é Ímpar.";
+    }
+}
+
+// Chamando o procedimento com um exemplo
+verificarParImpar(7);
+?>
