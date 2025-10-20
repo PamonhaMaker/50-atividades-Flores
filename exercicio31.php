ce620cb2cc4d87276a1934a51ae490a738eb6180
PamonhaMaker <gabrizdiniz@gmail.com>
2025-10-20 16:30:26 -0300

    Add saudacao function to greet users

--- exercicio31.php
+++ exercicio31.php
@@ -1,1 +1,20 @@
-
+<?php
+/**************************
+    Curso: Engenharia de Software
+    Disciplina: Linguagem e Técnicas de Programacão
+    Professor: Flores
+    Turma: ESOFT-2A
+    Componentes:
+    25142481-2 - Isabelli Azevedo
+    25324764-2- Gabriel Diniz
+    25016187-2- Michel Bueno Chacon 
+    25127107-2-Tiago Joslin
+    Data: 08 de Outubro de 2025
+    Descritivo: : Procedimento: Crie um procedimento chamado saudacao que imprima "Bem-vindo ao PHP!".
+    ***************************/
+function saudacao()
+{
+    echo "Bem-vindo ao PHP!";
+}
+saudacao();
+?>
