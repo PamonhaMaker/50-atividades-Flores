0c97b4edbb5b5f4a153dabacba95cc0367249995
PamonhaMaker <gabrizdiniz@gmail.com>
2025-10-20 15:55:22 -0300

    Add script to print multiples of 3 from 3 to 30
    This PHP script prints multiples of 3 from 3 to 30.

--- exercicio07.php
+++ exercicio07.php
@@ -1,1 +1,22 @@
+<?php
+    /**************************
+    Curso: Engenharia de Software
+    Disciplina: Linguagem e Técnicas de Programacão
+    Professor: Flores
+    Turma: ESOFT-2A
+    Componentes:
+    25142481-2 - Isabelli Azevedo
+    25324764-2- Gabriel Diniz
+    25016187-2- Michel Bueno Chacon 
+    25127107-2-Tiago Joslin
+    Data: 08 de Outubro de 2025
+    Descritivo: Faça um programa que imprima os múltiplos de 3, de 3 a 30.
+    *********/
+    
+$n = 1;
 
+while ($n <= 10) {
+    echo $n * 3 . "\n";
+    $n++;
+}
+?>
