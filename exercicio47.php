8aace3c95205b01e30a7c2828237c239d736cfc6
PamonhaMaker <gabrizdiniz@gmail.com>
2025-10-20 16:35:15 -0300

    Implement contarVogais function in exercicio47.php
    Add function to count vowels in a string

--- exercicio47.php
+++ exercicio47.php
@@ -1,1 +1,27 @@
-
+<?php
+/**************************
+    Curso: Engenharia de Software
+    Disciplina: Linguagem e Técnicas de Programacão
+    Professor: Flores
+    Turma: ESOFT-2A
+    Componentes:
+    25142481-2 - Isabelli Azevedo
+    25324764-2- Gabriel Diniz
+    25016187-2- Michel Bueno Chacon 
+    25127107-2-Tiago Joslin
+    Data: 08 de Outubro de 2025
+    Descritivo: : Função com String: Crie uma função contarVogais que receba uma string e retorne o número de vogais.
+    ***************************/
+function contarVogais($texto) {
+    $texto = strtolower($texto);
+    $contador = 0;
+for ($i = 0; $i < strlen($texto); $i++) {
+        if (in_array($texto[$i], ['a', 'e', 'i', 'o', 'u'])) {
+            $contador++;
+        }
+    }
+    return $contador;
+}
+$frase = "Teste De Mesa"; // é pra sair 5
+echo contarVogais($frase)
+?>
