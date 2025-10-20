c5d8baaa85832f969ce51892a8b224d9e241d2b7
PamonhaMaker <gabrizdiniz@gmail.com>
2025-10-20 15:47:02 -0300

    Add PHP program to print even numbers from 2 to 20
    Implemented a PHP program to print even numbers from 2 to 20.

--- exercicio02.php
+++ exercicio02.php
@@ -1,1 +1,19 @@
+<?php
+/**************************
+Curso: Engenharia de Software
+Disciplina: Linguagem e Técnicas de Programacão
+Professor: Flores
+Turma: ESOFT-2A
+Componentes:
+25142481-2 - Isabelli Azevedo
+25324764-2- Gabriel Diniz
+25016187-2- Michel Bueno Chacon 
+25127107-2-Tiago Joslin
+Data: 08 de Outubro de 2025
+Descritivo: Crie um programa que use for para imprimir os números pares de 2 a 20
+    ***************************/
 
+    for ($a = 2; $a <= 20; $a+=2) {
+        echo $a . "\n"; 
+    }
+?>
