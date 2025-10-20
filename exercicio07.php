a366555bb4b96c6095b10fbe35e96f82cce7d220
PamonhaMaker <gabrizdiniz@gmail.com>
2025-10-20 15:57:01 -0300

    Delete program for printing multiples of 3
    Removed the program that prints multiples of 3 from 3 to 30.

--- exercicio07.php
+++ exercicio07.php
@@ -9,14 +9,3 @@
     25324764-2- Gabriel Diniz
     25016187-2- Michel Bueno Chacon 
     25127107-2-Tiago Joslin
-    Data: 08 de Outubro de 2025
-    Descritivo: Faça um programa que imprima os múltiplos de 3, de 3 a 30.
-    *********/
-    
-$n = 1;
-
-while ($n <= 10) {
-    echo $n * 3 . "\n";
-    $n++;
-}
-?>
