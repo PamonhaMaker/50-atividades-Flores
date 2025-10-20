28cfea5316ebb53eda8396b0ceed9f66bd774734
PamonhaMaker <gabrizdiniz@gmail.com>
2025-10-20 17:07:48 -0300

    Add PHP script to print cities using foreach
    Implements a PHP script to print cities from an array.

--- exercicio18.php
+++ exercicio18.php
@@ -1,1 +1,23 @@
+<?php
+    /**************************
+    Curso: Engenharia de Software
+    Disciplina: Linguagem e Técnicas de Programacão
+    Professor: Flores
+    Turma: ESOFT-2A
+    Componentes:
+    25142481-2 - Isabelli Azevedo
+    25324764-2- Gabriel Diniz
+    25016187-2- Michel Bueno Chacon 
+    25127107-2-Tiago Joslin
+    Data: 08 de Outubro de 2025
+    Descritivo: Usando o array de cidades do exercício 16, imprima cada cidade em uma nova linha usando foreach.
+    ***************************/
+// Criando o array com as cidades
+$cidades = array("São Paulo", "Rio de Janeiro", "Belo Horizonte", "Curitiba", "Salvador");
+
+// Usando foreach para percorrer e imprimir cada cidade
+foreach ($cidades as $cidade) {
+    echo $cidade . "<br>";
+}
+?>
 
