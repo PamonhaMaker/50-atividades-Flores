4b58a1d9e33f3d8f9f69e7565a54fd198c292c7e
PamonhaMaker <gabrizdiniz@gmail.com>
2025-10-20 17:11:46 -0300

    Update exercicio22.php

--- exercicio22.php
+++ exercicio22.php
@@ -1,1 +1,11 @@
+$aluno = array(
+    "nome" => "Gabriel Diniz",
+    "idade" => 18,
+    "curso" => "Engenharia de Software"
+);
 
+// Usando foreach para percorrer e imprimir no formato "chave: valor"
+foreach ($aluno as $chave => $valor) {
+    echo $chave . ": " . $valor . "<br>";
+}
+?>
