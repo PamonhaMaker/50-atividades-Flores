6ac61e30f1837cfb66735f398550779ed4632cd5
PamonhaMaker <gabrizdiniz@gmail.com>
2025-10-20 16:44:16 -0300

    Update exercicio21.php

--- exercicio21.php
+++ exercicio21.php
@@ -1,1 +1,24 @@
+<?php
+/**************************
+Curso: Engenharia de Software
+Disciplina: Linguagem e Técnicas de Programacão
+Professor: Flores
+Turma: ESOFT-2A
+Componentes:
+25142481-2 - Isabelli Azevedo
+25324764-2- Gabriel Diniz
+25016187-2- Michel Bueno Chacon 
+25127107-2-Tiago Joslin
+Data: 08 de Outubro de 2025
+Descritivo: Array/FOR: Crie um array com os 7 dias da semana e imprima-os usando um laço for.
+***************************/
+$aluno = array(
+    "nome" => "Gabriel Diniz",
+    "idade" => 21,
+    "curso" => "Análise e Desenvolvimento de Sistemas"
+);
 
+echo "Nome: " . $aluno["nome"] . "<br>";
+echo "Idade: " . $aluno["idade"] . "<br>";
+echo "Curso: " . $aluno["curso"] . "<br>";
+?>
