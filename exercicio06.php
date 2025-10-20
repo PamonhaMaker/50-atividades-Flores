019c7beaf86adc8d58cb5131b0619a95d72c9afe
PamonhaMaker <gabrizdiniz@gmail.com>
2025-10-20 15:53:41 -0300

    Create exercicio06.php to print numbers 1 to 10
    Add PHP script to print numbers from 1 to 10 using a while loop.

--- exercicio06.php
+++ exercicio06.php
@@ -1,1 +1,22 @@
+<?php
+    /**************************
+    Curso: Engenharia de Software
+    Disciplina: Linguagem e Técnicas de Programacão
+    Professor: Flores
+    Turma: ESOFT-2A
+    Componentes:
+    25142481-2 - Isabelli Azevedo
+    25324764-2- Gabriel Diniz
+    25016187-2- Michel Bueno Chacon 
+    25127107-2-Tiago Joslin
+    Data: 08 de Outubro de 2025
+    Descritivo: Escreva um programa que use while para imprimir os números de 1 a 10
+    ***************************/
+    
+$n = 1;
 
+while ($n <= 10) {
+    echo $n . "\n";
+    $n++;
+}
+?>
