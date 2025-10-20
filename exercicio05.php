979226259f4e81d98724220c57ae37add5a0329a
PamonhaMaker <gabrizdiniz@gmail.com>
2025-10-20 15:53:01 -0300

    Add sum calculation for numbers 1 to 100
    Implemented a program to calculate the sum of numbers from 1 to 100.

--- exercicio05.php
+++ exercicio05.php
@@ -1,1 +1,23 @@
+<?php
+    /**************************
+    Curso: Engenharia de Software
+    Disciplina: Linguagem e Técnicas de Programacão
+    Professor: Flores
+    Turma: ESOFT-2A
+    Componentes:
+    25142481-2 - Isabelli Azevedo
+    25324764-2- Gabriel Diniz
+    25016187-2- Michel Bueno Chacon 
+    25127107-2-Tiago Joslin
+    Data: 08 de Outubro de 2025
+    Descritivo: Crie um programa que calcule e imprima a soma dos números de 1 a 100.    
+***************************/
+    
+$s = 0;
 
+for ($a = 1; $a <= 100; $a++) {
+    $s += $a;
+}
+
+echo "A soma dos números de 1 a 100 é: $s\n";
+?>
