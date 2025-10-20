549740fc24ffbc3d755c1ea360d662b0929f9ec9
PamonhaMaker <gabrizdiniz@gmail.com>
2025-10-20 16:14:26 -0300

    Update exercicio36.php

--- exercicio36.php
+++ exercicio36.php
@@ -1,1 +1,23 @@
+<?php
+    /**********
+    Curso: Engenharia de Software
+    Disciplina: Linguagem e Técnicas de Programacão
+    Professor: Flores
+    Turma: ESOFT-2A
+    Componentes:
+    25142481-2 - Isabelli Azevedo
+    25324764-2- Gabriel Diniz
+    25016187-2- Michel Bueno Chacon 
+    25127107-2-Tiago Joslin
+    Data: 08 de Outubro de 2025
+    Descritivo: : Crie um procedimento desenharLinha que imprima uma linha de 20 hifens --------------------.
+    *********/
+    
 
+// Definindo o procedimento desenharLinha
+function desenharLinha() {
+    echo str_repeat("-", 20) . "\n";
+}
+
+// Exemplo de uso
+desenharLinha();
