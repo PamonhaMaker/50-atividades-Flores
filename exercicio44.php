25fbb726373a87a65fc7a681f917c706c4a610cf
PamonhaMaker <gabrizdiniz@gmail.com>
2025-10-20 16:16:53 -0300

    Add calcularAreaRetangulo function
    Implement function to calculate rectangle area.

--- exercicio44.php
+++ exercicio44.php
@@ -1,1 +1,27 @@
+<?php
+    /**********
+    Curso: Engenharia de Software
+    Disciplina: Linguagem e Técnicas de Programacão
+    Professor: Flores
+    Turma: ESOFT-2A
+    Componentes:
+    25142481-2 - Isabelli Azevedo
+    25324764-2- Gabriel Diniz
+    25016187-2- Michel Bueno Chacon 
+    25127107-2-Tiago Joslin
+    Data: 08 de Outubro de 2025
+    Descritivo: : Escreva uma função calcular Area Retangulo que receba a largura e a altura e retorne a área.
+     *********/
 
+function calcularAreaRetangulo($largura, $altura) {
+    return $largura * $altura;
+}
+
+// Exemplo de uso
+$largura = 5;
+$altura = 10;
+$area = calcularAreaRetangulo($largura, $altura);
+
+echo "A área do retângulo é: " . $area . "\n";
+
+?>
