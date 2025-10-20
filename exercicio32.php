54b488c48ce43672c228742fcf9ddb76cd9dae07
PamonhaMaker <gabrizdiniz@gmail.com>
2025-10-20 16:13:37 -0300

    Implement greeting function for user names
    Added a greeting procedure to welcome users by name.

--- exercicio32.php
+++ exercicio32.php
@@ -1,1 +1,26 @@
+<?php
+    /**********
+    Curso: Engenharia de Software
+    Disciplina: Linguagem e Técnicas de Programacão
+    Professor: Flores
+    Turma: ESOFT-2A
+    Componentes:
+    25142481-2 - Isabelli Azevedo
+    25324764-2- Gabriel Diniz
+    25016187-2- Michel Bueno Chacon 
+    25127107-2-Tiago Joslin
+    Data: 08 de Outubro de 2025
+    Descritivo: : Crie um procedimento saudação Usuário que receba um nome como parâmetro e imprima "Olá, [nome]!".
+    *********/
+    
 
+// Definindo o procedimento (função sem retorno)
+function saudacaoUsuario($nome) {
+    echo "Olá, $nome!\n";
+}
+
+// Exemplo de uso
+saudacaoUsuario("Maria");
+saudacaoUsuario("João");
+
+?>
