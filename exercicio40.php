bd15199f7179f27a6b96499a8519e916574c5b36
PamonhaMaker <gabrizdiniz@gmail.com>
2025-10-20 16:16:02 -0300

    Add function to print car details
    Implement a function to print car details from an associative array.

--- exercicio40.php
+++ exercicio40.php
@@ -1,1 +1,31 @@
+<?php
+    /**********
+    Curso: Engenharia de Software
+    Disciplina: Linguagem e Técnicas de Programacão
+    Professor: Flores
+    Turma: ESOFT-2A
+    Componentes:
+    25142481-2 - Isabelli Azevedo
+    25324764-2- Gabriel Diniz
+    25016187-2- Michel Bueno Chacon 
+    25127107-2-Tiago Joslin
+    Data: 08 de Outubro de 2025
+    Descritivo: : Faça um procedimento que receba um array associativo de um carro (marca, modelo, ano) e imprima seus dados.
+     *********/
 
+// Procedimento que recebe um array associativo e imprime os dados do carro
+function imprimirDadosCarro($carro) {
+    echo "Marca: " . $carro['marca'] . "\n";
+    echo "Modelo: " . $carro['modelo'] . "\n";
+    echo "Ano: " . $carro['ano'] . "\n";
+}
+
+// Exemplo de uso
+$meuCarro = array(
+    "marca" => "Toyota",
+    "modelo" => "Corolla",
+    "ano" => 2020
+);
+
+imprimirDadosCarro($meuCarro);
+?>
