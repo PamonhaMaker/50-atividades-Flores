b46c1d4d72f1969be030547462dacddc70591c0c
PamonhaMaker <gabrizdiniz@gmail.com>
2025-10-20 15:51:45 -0300

    Create countdown script in exercicio04.php
    Added a PHP script to count down from 10 to 1.

--- exercicio04.php
+++ exercicio04.php
@@ -1,1 +1,19 @@
+<?php
+    /**************************
+    Curso: Engenharia de Software
+    Disciplina: Linguagem e Técnicas de Programacão
+    Professor: Flores
+    Turma: ESOFT-2A
+    Componentes:
+    25142481-2 - Isabelli Azevedo
+    25324764-2- Gabriel Diniz
+    25016187-2- Michel Bueno Chacon 
+    25127107-2-Tiago Joslin
+    Data: 08 de Outubro de 2025
+    Descritivo: Escreva um programa que use for para contar de 10 a 1 em ordem decrescente
+    *********/
+    for ($i = 10; $i >= 1; $i--) {
+        echo $i . " ";
+    }
 
+?>
