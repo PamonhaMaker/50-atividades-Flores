7179d0cd2fb3950fc9bd49b3a2d22285204ded6a
PamonhaMaker <gabrizdiniz@gmail.com>
2025-10-20 16:22:35 -0300

    Add do-while loop for printing numbers
    Implement do-while loop to print numbers 1 to 10

--- exercicio11.php
+++ exercicio11.php
@@ -1,1 +1,20 @@
-
+<?php
+/**************************
+    Curso: Engenharia de Software
+    Disciplina: Linguagem e Técnicas de Programacão
+    Professor: Flores
+    Turma: ESOFT-2A
+    Componentes:
+    25142481-2 - Isabelli Azevedo
+    25324764-2- Gabriel Diniz
+    25016187-2- Michel Bueno Chacon 
+    25127107-2-Tiago Joslin
+    Data: 08 de Outubro de 2025
+    Descritivo: : DO-WHILE: Escreva um programa que use do-while para imprimir os números de 1 a 10.
+    ***************************/
+$numero = 1;
+do {
+    echo "$numero"." ";
+    $numero++;
+} while ($numero <=10);
+?>
