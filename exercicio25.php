daf109dfd12259177e12baa79d0c6b582ddc49a1
PamonhaMaker <gabrizdiniz@gmail.com>
2025-10-20 16:45:12 -0300

    Create exercicio25.php to count array elements
    Add a PHP script to create an array and count its elements.

--- exercicio25.php
+++ exercicio25.php
@@ -1,1 +1,25 @@
+<?php
+/**************************
+Curso: Engenharia de Software
+Disciplina: Linguagem e Técnicas de Programacão
+Professor: Flores
+Turma: ESOFT-2A
+Componentes:
+ 25142481-2 - Isabelli Azevedo
+ 25324764-2 - Gabriel Diniz
+ 25016187-2 - Michel Bueno Chacon
+ 25127107-2 - Tiago Joslin
+Data: 08 de Outubro de 2025
+Descritivo: Array/Funções Nativas - Crie um array de números e use a função 
+count() para mostrar quantos elementos ele possui
+**************************/
 
+// Criação de um array de números
+$num = array(10, 20, 30, 40, 50, 60);
+
+// Uso da função count() para contar quantos elementos o array possui
+$qnt = count($num);
+
+// Exibição do resultado
+echo "O array possui " . $qnt . " elementos.";
+?>
