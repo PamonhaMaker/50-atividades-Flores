dfd8a99f222aca18064c88a24181a8d75e7a9495
PamonhaMaker <gabrizdiniz@gmail.com>
2025-10-20 17:01:39 -0300

    Update exercicio15.php

--- exercicio15.php
+++ exercicio15.php
@@ -11,25 +11,25 @@ Componentes:
  25004001-4 - Sergio Roberto Gularte
  25004001-5 - Lauana Prado de Andrade
 Data: 08 de Outubro de 2025
-Descritivo: Escreva um programa que calcule a soma de dois números
+Descritivo: Crie um programa para encontrar e imprimir todos os números primos entre 1 e 50.
 *******************************************************************************/
-$numero = 7;
-$contador = 0;
+<?php
 
-if($numero > 50)
-    echo "Apenas Menor que 50";
- else{  
-    for($i=1; $i<=$numero; $i++){
-        if($numero % $i == 0){
-            $contador ++;
+// Percorre os números de 2 até 50
+for ($num = 2; $num <= 50; $num++) {
+    $ehPrimo = true; // Assume que o número é primo
+
+    // Verifica se o número tem divisores além de 1 e ele mesmo
+    for ($div = 2; $div <= sqrt($num); $div++) {
+        if ($num % $div == 0) {
+            $ehPrimo = false; // Se for divisível, não é primo
+            break;
         }
     }
 
-    if($contador == 2){
-        echo "Primo";
-    }
-    else{
-        echo "Não é primo";
+    // Se continuar sendo primo, imprime
+    if ($ehPrimo) {
+        echo $num . "<br>";
     }
 }
 ?>
