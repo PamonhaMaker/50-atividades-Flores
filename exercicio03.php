e7b77915c4d80745ee2a00c081d016a4da33fb66
PamonhaMaker <gabrizdiniz@gmail.com>
2025-10-20 15:50:28 -0300

    Update exercicio03.php

--- exercicio03.php
+++ exercicio03.php
@@ -1,1 +1,20 @@
+<?php
+    /**************************
+    Curso: Engenharia de Software
+    Disciplina: Linguagem e Técnicas de Programacão
+    Professor: Flores
+    Turma: ESOFT-2A
+    Componentes:
+    25142481-2 - Isabelli Azevedo
+    25324764-2- Gabriel Diniz
+    25016187-2- Michel Bueno Chacon 
+    25127107-2-Tiago Joslin
+    Data: 08 de Outubro de 2025
+    Descritivo: Faça um programa que exiba a tabuada do 5, de 1 a 10
+    ***************************/
 
+for ($a = 1; $a <= 10; $a++) {
+    $r = 5 * $a;
+    echo "5 x $a = $r\n";
+}
+?>
