8e98b8bb2f0a6029d6e755b7fba08c11a8a91097
PamonhaMaker <gabrizdiniz@gmail.com>
2025-10-20 16:33:39 -0300

    Implement converterParaMaiusculas function
    Added a function to convert a string to uppercase.

--- exercicio43.php
+++ exercicio43.php
@@ -1,1 +1,21 @@
-
+<?php
+/**************************
+    Curso: Engenharia de Software
+    Disciplina: Linguagem e Técnicas de Programacão
+    Professor: Flores
+    Turma: ESOFT-2A
+    Componentes:
+    25142481-2 - Isabelli Azevedo
+    25324764-2- Gabriel Diniz
+    25016187-2- Michel Bueno Chacon 
+    25127107-2-Tiago Joslin
+    Data: 08 de Outubro de 2025
+    Descritivo: : Função: Elabore uma função converterParaMaiusculas que receba uma string e a retorne em letras maiúsculas.
+    ***************************/
+function converterParaMaiusculas($texto){
+    return strtoupper($texto);
+}
+$texto = "letra minúscula para maiuscula";
+$maisculo = converterParaMaiusculas($texto);
+echo $maisculo;
+?>
