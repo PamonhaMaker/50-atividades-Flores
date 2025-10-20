762310597a77301f79637ac104c36ee4a6b804e6
PamonhaMaker <gabrizdiniz@gmail.com>
2025-10-20 15:44:23 -0300

    Implement number printing from 1 to 10
    Add a program to print numbers from 1 to 10 using a for loop.

--- exercicio01.php
+++ exercicio01.php
@@ -1,1 +1,19 @@
+<?php
+/**************************
+Curso: Engenharia de Software
+Disciplina: Linguagem e Técnicas de Programacão
+Professor: Flores
+Turma: ESOFT-2A
+Componentes:
+25142481-2 - Isabelli Azevedo
+25324764-2- Gabriel Diniz
+25016187-2- Michel Bueno Chacon 
+25127107-2-Tiago Joslin
+Data: 08 de Outubro de 2025
+Descritivo: Escreva um programa que use for para imprimir os números de 1 a 10.
+ ***************************/
 
+    for ($a = 1; $a <= 10; $a++) {
+        echo $a . "\n"; 
+    }
+?>
