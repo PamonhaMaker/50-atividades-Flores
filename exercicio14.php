d562a974adff367692d2498863287f102713cee4
PamonhaMaker <gabrizdiniz@gmail.com>
2025-10-20 16:54:33 -0300

    Implement number input loop with header comments
    Add header comments and loop for number input

--- exercicio14.php
+++ exercicio14.php
@@ -1,1 +1,20 @@
-
+<?php
+    /**************************
+    Curso: Engenharia de Software
+    Disciplina: Linguagem e Técnicas de Programacão
+    Professor: Flores
+    Turma: ESOFT-2A
+    Componentes:
+    25142481-2 - Isabelli Azevedo
+    25324764-2- Gabriel Diniz
+    25016187-2- Michel Bueno Chacon 
+    25127107-2-Tiago Joslin
+    Data: 08 de Outubro de 2025
+    Descritivo: Escreva um programa que leia um número e o imprima. O programa deve repetir até que o número lido seja negativo.
+    ***************************/
+    
+do {
+$contador = readline ("Digite um número ou -1 para encerrar: \n");
+   
+} while ($contador >= 0);
+?>
