1fd0bcfc7f1ddce2037f8dc4383adcce934658f6
PamonhaMaker <gabrizdiniz@gmail.com>
2025-10-20 16:31:38 -0300

    Implement imprimirArray function
    Add function to print elements of an array

--- exercicio35.php
+++ exercicio35.php
@@ -1,1 +1,22 @@
-
+<?php
+/**************************
+    Curso: Engenharia de Software
+    Disciplina: Linguagem e Técnicas de Programacão
+    Professor: Flores
+    Turma: ESOFT-2A
+    Componentes:
+    25142481-2 - Isabelli Azevedo
+    25324764-2- Gabriel Diniz
+    25016187-2- Michel Bueno Chacon 
+    25127107-2-Tiago Joslin
+    Data: 08 de Outubro de 2025
+    Descritivo: : Procedimento com Array: Crie um procedimento imprimirArray que receba um array e imprima todos os seus elementos.
+    ***************************/
+function imprimirArray($array) {
+    foreach ($array as $elemento) {
+        echo $elemento." ";
+    }
+}
+$Array = [10, 20, 30];
+imprimirArray($Array);
+?>
